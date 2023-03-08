d6643a0020e4aa951b8f128c4da3c0c62609d1bd
A <ajaygeorge@me.com>
2023-03-08 21:22:59 +0000

    In the updated code, BodyWrapper wraps the LandingPageWrapper component and sets the background to the default background color and the minimum height of the viewport. The LandingPageWrapper component sets the color to the default text color and centers the content vertically and horizontally.

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -10,11 +10,14 @@ import {
 import { styled } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
 
-const LandingPageWrapper = styled('div')(({ theme }) => ({
+const BodyWrapper = styled('div')(({ theme }) => ({
   background: theme.palette.background.default,
+  minHeight: '100vh',
+}));
+
+const LandingPageWrapper = styled('div')(({ theme }) => ({
   color: theme.palette.text.primary,
   transition: 'background 0.3s ease-in-out, color 0.3s ease-in-out',
-  height: '100vh',
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
@@ -35,69 +38,71 @@ function LandingPage({ toggleTheme, theme }) {
 
   return (
     <ThemeProvider theme={theme}>
-      <LandingPageWrapper>
-        <h1>Welcome to our Resume Builder and Cover Letter Generator</h1>
-        <div className="buttons">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleOptionClick('resume')}
-          >
-            Build a Resume
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleOptionClick('cover-letter')}
-          >
-            Generate a Cover Letter
-          </Button>
-        </div>
-        {selectedOption === 'resume' && (
-          <form onSubmit={handleFormSubmit}>
-            <TextField label="Current Resume" fullWidth />
-            <TextField
-              label="Details of job you are applying for"
-              fullWidth
-            />
-            <FormControl component="fieldset">
-              <RadioGroup>
-                <FormControlLabel
-                  value="yes"
-                  control={<Radio />}
-                  label="Roles summarized in bullet points?"
-                />
-                <FormControlLabel
-                  value="no"
-                  control={<Radio />}
-                  label="Roles not summarized in bullet points?"
-                />
-              </RadioGroup>
-            </FormControl>
-            <Button variant="contained" color="primary" type="submit">
-              Build Resume
+      <BodyWrapper>
+        <LandingPageWrapper>
+          <h1>Welcome to our Resume Builder and Cover Letter Generator</h1>
+          <div className="buttons">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleOptionClick('resume')}
+            >
+              Build a Resume
             </Button>
-          </form>
-        )}
-        {selectedOption === 'cover-letter' && (
-          <form onSubmit={handleFormSubmit}>
-            <TextField label="Resume" fullWidth />
-            <TextField
-              label="Current Cover Letter (optional)"
-              fullWidth
-              multiline
-              rows={4}
-            />
-            <TextField
-              label="Details of job you are applying for"
-              fullWidth
-            />
-            <Button variant="contained" color="primary" type="submit">
-              Generate Cover Letter
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleOptionClick('cover-letter')}
+            >
+              Generate a Cover Letter
             </Button>
-          </form>
-        )}
-      </LandingPageWrapper>
+          </div>
+          {selectedOption === 'resume' && (
+            <form onSubmit={handleFormSubmit}>
+              <TextField label="Current Resume" fullWidth />
+              <TextField
+                label="Details of job you are applying for"
+                fullWidth
+              />
+              <FormControl component="fieldset">
+                <RadioGroup>
+                  <FormControlLabel
+                    value="yes"
+                    control={<Radio />}
+                    label="Roles summarized in bullet points?"
+                  />
+                  <FormControlLabel
+                    value="no"
+                    control={<Radio />}
+                    label="Roles not summarized in bullet points?"
+                  />
+                </RadioGroup>
+              </FormControl>
+              <Button variant="contained" color="primary" type="submit">
+                Build Resume
+              </Button>
+            </form>
+          )}
+          {selectedOption === 'cover-letter' && (
+            <form onSubmit={handleFormSubmit}>
+              <TextField label="Resume" fullWidth />
+              <TextField
+                label="Current Cover Letter (optional)"
+                fullWidth
+                multiline
+                rows={4}
+              />
+              <TextField
+                label="Details of job you are applying for"
+                fullWidth
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Generate Cover Letter
+              </Button>
+            </form>
+          )}
+        </LandingPageWrapper>
+      </BodyWrapper>
     </ThemeProvider>
   );
 }
